a81d9cc623bd61097e83ae02acb59d29bc4688f0
unknown <cdonohoe1374@gmail.com>
2020-01-09 14:56:55 +0000

    first commit

---
+++ src/server.java
@@ -0,0 +1,142 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.*;
+//test
+public class server {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ServerSocket listener;
+		int clientid=0;
+		try 
+		{
+			 listener = new ServerSocket(10000,10);
+			 
+			 while(true)
+			 {
+				System.out.println("Main thread listening for incoming new connections");
+				Socket newconnection = listener.accept();
+				
+				System.out.println("New connection received and spanning a thread");
+				Connecthandler t = new Connecthandler(newconnection, clientid);
+				clientid++;
+				t.start();
+			 }
+			
+		} 
+		
+		catch (IOException e) 
+		{
+			System.out.println("Socket not opened");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
+
+
+class Connecthandler extends Thread
+{
+
+	Socket individualconnection;
+	int socketid;
+	ObjectOutputStream out;
+	ObjectInputStream in;
+	String message;
+	int result;
+	
+	public Connecthandler(Socket s, int i)
+	{
+		individualconnection = s;
+		socketid = i;
+	}
+	
+	void sendMessage(String msg)
+	{
+		try{
+			out.writeObject(msg);
+			out.flush();
+			System.out.println("client>" + msg);
+		}
+		catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+	}
+	
+	public void run()
+	{
+		
+		try 
+		{
+		
+			out = new ObjectOutputStream(individualconnection.getOutputStream());
+			out.flush();
+			in = new ObjectInputStream(individualconnection.getInputStream());
+			System.out.println("Connection"+ socketid+" from IP address "+individualconnection.getInetAddress());
+		
+		 
+			//Commence
+			do 
+			{
+			
+				do
+				{
+					sendMessage("Please Enter 1 to Login or 2 to Register as Club or 3 to Register as an Agent");
+					message = (String)in.readObject();
+				}while(!message.equals("1")&&!message.equals("2")&&!message.equals("3"));
+			
+				if(message.equals("1"))
+				{
+					//login();
+				}
+			
+				else if(message.equals("2"))
+				{
+					//club.register();	
+				}
+				
+				else if(message.equals("3"))
+				{
+					//agent.register();	
+				}
+			
+			
+				sendMessage("Y to repeat or N to terminate");
+			
+				message = (String)in.readObject();
+			}while(message.equalsIgnoreCase("Y"));
+		
+		}
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		finally
+		{
+			try 
+			{
+				out.close();
+				in.close();
+				individualconnection.close();
+			}
+			
+	
+			catch (IOException e) 
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
+}
